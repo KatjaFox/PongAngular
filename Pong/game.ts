903336621b90b845ec37dfa2aaca2ea40065ff1c
KatjaFox <cathrinefox515@gmail.com>
2019-09-19 17:37:06 +0200

    pong folder added

---
+++ Pong/game.ts
@@ -0,0 +1,9 @@
+export class Game {
+    public score: number;
+    public comment: string;
+
+    constructor(score: number, comment: string) {
+        this.score = score;
+        this.comment = comment;
+    }
+}
